fb5f4c2fa6fd056231c3164d2a046925a75d0d8a
Brett Hinton <brett@hintonweb.com>
2022-04-02 22:11:39 -0700

    Update work-machine-setup.ps1

--- work-machine-setup.ps1
+++ work-machine-setup.ps1
@@ -3,3 +3,4 @@ winget install Microsoft.VisualStudio.2022.Enterprise
 winget install Microsoft.Powershell
 winget install royalts ## Get License key from Spark
 ## Install Sophos SSL VPN application to get access to hosting VPN
+## Download Todoist - the winget package seems to be the legacy one
