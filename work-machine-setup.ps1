c0b13f50fbcbf246aace183d152de59cd09bc277
Brett Hinton <brett@hintonweb.com>
2022-04-02 21:22:46 -0700

    Update work-machine-setup.ps1

--- work-machine-setup.ps1
+++ work-machine-setup.ps1
@@ -2,4 +2,4 @@ winget install Microsoft.VisualStudioCode
 winget install Microsoft.VisualStudio.2022.Enterprise
 winget install Microsoft.Powershell
 winget install royalts ## Get License key from Spark
-## Install Sophos Connect application to get access to hosting VPN
+## Install Sophos SSL VPN application to get access to hosting VPN
